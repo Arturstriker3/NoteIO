079ef822cf7ab0c8f50e6af2d3b8c6e4a5c3ec32
Arturstriker3 <artur.danielgama@gmail.com>
2024-01-23 02:51:22 -0300

    Fix: Formatação Datas

--- src/views/Menu/Slots/AddScreen/Js/handleDateInput.js
+++ src/views/Menu/Slots/AddScreen/Js/handleDateInput.js
@@ -1,3 +1,4 @@
+// handleDateInput.js
 
 export default function (note) {
   const currentDate = new Date();
@@ -5,16 +6,5 @@ export default function (note) {
 
   if (enteredDate < currentDate) {
     note.reminder = currentDate.toISOString().split('T')[0];
-  } else {
-    const formattedDate = formatDate(enteredDate);
-    note.reminder = formattedDate;
   }
-}
-
-function formatDate(date) {
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${day}/${month}/${year}`;
 }
--- src/views/Menu/Slots/NoteScreen/Js/noteDates.js
+++ src/views/Menu/Slots/NoteScreen/Js/noteDates.js
@@ -1,4 +1,3 @@
-
 export const formattedCreationDate = (activeNote) => {
   if (activeNote && activeNote.timestamp) {
     const timestamp = activeNote.timestamp;
@@ -17,7 +16,6 @@ export const formattedReminderDate = (activeNote) => {
   if (activeNote && activeNote.reminder) {
     const timestamp = activeNote.reminder;
     const date = new Date(timestamp);
-
     const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
     const month = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
 
